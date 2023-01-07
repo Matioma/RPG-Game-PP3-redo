66a25be0d609b2376ff9028050b9535ff44a56a1
Artiom Matievschi <Artiom.Matievschi@gmail.com>
2023-01-07 19:41:35 +0100

    Create a script for building

---
+++ scripts/buildProject.bat
@@ -0,0 +1,10 @@
+
+SET mypath=%~dp0
+echo %mypath:~0,-1%
+
+set src=%1
+set dest=%2
+
+echo  %arg1% %arg2%
+
+RunUAT BuildCookRun -project="%mypath:~0,-1%/%src%" -platform=Win64 -clientconfig=DebugGame -build -cook -stage -archive -archivedirectory="%mypath:~0,-1%/%dest%"
