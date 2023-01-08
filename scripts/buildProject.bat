1e03acd0855bf98a22ea680cf964b4d4dcd79aa9
Artiom Matievschi <Artiom.Matievschi@gmail.com>
2023-01-08 09:58:40 +0100

    Zip Project

--- scripts/buildProject.bat
+++ scripts/buildProject.bat
@@ -8,3 +8,6 @@ set dest=%2
 echo  %arg1% %arg2%
 
 RunUAT BuildCookRun -project="%mypath:~0,-1%/%src%" -platform=Win64 -clientconfig=DebugGame -build -cook -stage -archive -archivedirectory="%mypath:~0,-1%/%dest%"
+
+cd "%mypath:~0,-1%/%dest%"
+powershell Compress-Archive . publish.zip
